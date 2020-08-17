c6033bdd5103a2760d6758d37a3cc9a38cad7d11
rubens-lavor <haryson07@gmail.com>
2020-08-17 00:15:59 -0300

    Comunicação com api. lang pt-br.

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,22 @@ import React, { Component } from "react";
 import api from "../../services/api";
 
 export default class Main extends Component {
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    //quando se usa os componetes pertencentes ao react
+    //pode se usar o modelo tradicional de função.
+    //porém quando a função é criada, é preciso usar uma
+    //Arrow function, senão a função não consegue ver o escopo
+    // do this.
+    
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        console.log(response.data.docs);
+    }
+
     render() {
         return (
             <h1>OIII GAlera do Mundo!</h1>
