eb1d2e090b01e28d0522b3fb71c9ca1592684d34
rubens-lavor <haryson07@gmail.com>
2020-08-17 00:06:05 -0300

    Component Main. Install axios

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 
 import Header from "./components/Hearder/index";
-
+import Main from "./pages/main";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/pages/main/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import api from "../../services/api";
+
+export default class Main extends Component {
+    render() {
+        return (
+            <h1>OIII GAlera do Mundo!</h1>
+        );
+    }
+};
