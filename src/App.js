801238e747a4ff48e8122f4634b9d15d1c8d7f4a
rubens-lavor <haryson07@gmail.com>
2020-08-16 23:44:06 -0300

    Componente Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 
+import Header from "./components/Hearder/index";
+
+
 function App() {
   return (
     <div className="App">
-      <h1>oi mundo!</h1>
+      <Header />
     </div>
   );
 }
